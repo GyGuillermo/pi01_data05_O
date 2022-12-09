9e6d3a22aa41fd19fe86c99ae580e1015410bf11
Guillermo <gylescano@gmail.com>
2022-12-09 08:03:42 -0300

    En la rama master  Tu rama está actualizada con 'origin/master'.  Cambios a ser confirmados: 	modificados:     Analisis/pi01_data05.ipynb 	modificados:     app/main.py

--- app/main.py
+++ app/main.py
@@ -21,6 +21,11 @@ async def get_max_duration(año:int,plataforma:str,unit_time:str):
 # Get para obtener cantidad de películas y series (separado) por plataforma
 @app.get("/get_count_plataform/")
 async def count_plataforma(plataforma:str):
+    import sys
+    import os
+    import pandas as pd
+    import numpy as np
+    url ='/home/guille/Escritorio/PI01_DATA05/Analisis/df_plataformas_concatenadas.csv'
     df = pd.read_csv(url)
     df_plataforma = df.query("plataforma == @plataforma")
     contador = df_plataforma.type.value_counts(sort=True)
@@ -29,6 +34,11 @@ async def count_plataforma(plataforma:str):
 # Get para obtener cantidad de veces que se repite un género y plataforma con mayor frecuencia del mismo
 @app.get("/get_listedin/")
 async def get_listedin (plataforma:str,categoria:str):
+    import sys
+    import os
+    import pandas as pd
+    import numpy as np
+    url ='/home/guille/Escritorio/PI01_DATA05/Analisis/df_plataformas_concatenadas.csv'
     df = pd.read_csv(url)
     df_plataforma = df.query("plataforma == @plataforma")
     return len(df_plataforma[df_plataforma.listed_in.str.contains(categoria)])
@@ -36,6 +46,11 @@ async def get_listedin (plataforma:str,categoria:str):
 # Get para obtener actor que más se repite según plataforma y año
 @app.get("/get_actor/")
 async def get_actor(plataforma:str,año:int):
+    import sys
+    import os
+    import pandas as pd
+    import numpy as np
+    url ='/home/guille/Escritorio/PI01_DATA05/Analisis/df_plataformas_concatenadas.csv'
     df = pd.read_csv(url)
     df_plataforma = df.query("plataforma == @plataforma and release_year == @año")
     new_df = pd.DataFrame(df_plataforma["cast"].str.split(',', expand=True).stack(), columns=["C1"])
